b2d1c6b0a8c4410bee28d883802a572683705a85
Matteo Collina <matteo.collina@gmail.com>
2010-07-02 12:36:18 +0200

    Disabled transactional fixtures as we are on mongo.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,5 +23,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, comment the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  
+  # set to false because we are using mongoid
+  config.use_transactional_fixtures = false 
 end
